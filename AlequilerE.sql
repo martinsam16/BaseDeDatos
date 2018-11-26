778e7ce32cf3080dcd77d02372268a0caa4ec1d1
MartinSamanArata2018 <martin.saman@vallegrande.edu.pe>
2018-11-26 08:48:25 -0500

    gg

---
+++ AlequilerE.sql
@@ -0,0 +1,9 @@
+/*create database Alquiler char set utf-8;*/
+use dbAlquiler;
+create table if not exists persona(
+    codper int PRIMARY key auto_increment,
+    nomper VARCHAR(100),
+    apeper VARCHAR(100),
+    dniper VARCHAR(8),
+    telper VARCHAR(9)
+);
