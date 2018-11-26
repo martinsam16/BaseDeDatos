d6853247ec98fd2ab953a185ff3e04202b7551fe
MartinSamanArata2018 <martin.saman@vallegrande.edu.pe>
2018-11-26 09:45:04 -0500

    CREADA LA TABLA UBIGEO

--- AlequilerE.sql
+++ AlequilerE.sql
@@ -7,6 +7,17 @@ select database();
 drop TABLE persona;
 /*Mostrar tablas*/
 show TABLES;
+/*Verificar la estructura de una tabla*/
+show columns in UBIGEO;
+describe persona;
+
+create TABLE if not exists UBIGEO(
+    CODUBI CHAR(6),
+    DISTUBI VARCHAR(100),
+    PROVUBI VARCHAR(100),
+    DPTOUBI VARCHAR(100),
+    CONSTRAINT COIDUBI_PK PRIMARY KEY (CODUBI)
+);
 
 create table if not exists persona(
     codper int auto_increment,
