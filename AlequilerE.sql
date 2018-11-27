04136f6f6a98cd747dd8e64164ccee327125a2ae
MartinSamanArata2018 <martin.saman@vallegrande.edu.pe>
2018-11-27 13:16:01 -0500

    gg

--- AlequilerE.sql
+++ AlequilerE.sql
@@ -1,34 +1,86 @@
-create database dbAlquiler CHAR set utf8;
+/*show databases;*/
+/*create database dbAlquiler char set utf-8;*/
 use dbAlquiler;
-describe ubigeo;
-select * from ubigeo;
-insert into ubigeo (codubi, distubi, provubi, dptoiubi)
-	values ('140307','San Vicente de Cañete','Cañete','Lima'),
-    ('140402','Calango','Cañete','Lima'),
-    ('140403','Cerro Azul','Cañete','Lima'),
-    ('140404','Coayllo','Cañete','Lima'),
-    ('140405','Chilca','Cañete','Lima'),
-    ('140406','Imperial','Cañete','Lima'),
-    ('140407','Lunahuaná','Cañete','Lima'),
-    ('140408','Mala','Cañete','Lima'),
-    ('140409','Nuevo Imperial','Cañete','Lima'),
-    ('140410','Pacarán','Cañete','Lima');
- 
- describe estado_vivienda;
- select * from estado_vivienda;
-insert into estado_vivienda (codestviv, descestviv) values (1,'Muy bueno'), (2,'Excelente'), (3,'Bueno'), (4,'Remodelado'), (5,'A Remodelar');
-
-describe tipo_vivienda;
-select * from tipo_vivienda;
-insert into tipo_vivienda (codtipviv, desctipviv) values 
-	(1,'Casa'),
-    (2,'Casa de Playa'),
-    (3,'Condominio'),
-    (4,'Casa de Campo'),
-    (5,'Casa de Playa en Condominio');
-    
+/*Mostrar database en uso*/
+select database();
+/*Eliminar tabla*/
+drop TABLE persona;
+/*Mostrar tablas*/
+show TABLES;
+/*Verificar la estructura de una tabla*/
+show columns in UBIGEO;
 describe persona;
+DESCRIBE VIVIENDA;
 
-insert into persona (codper,nomper,apepatper,apematper,dniper,dirper,ubigeo_codubi,telef1per,telef2per,sexoper,tipoper,usuper,passper)
-	values (1,'Martín Alexis','Samán','Arata','72720455','Urb. Valle Hermoso Mz. D Lt 21', '140406','974091056','-','M','P','admin','admin'),
-    (2,'Alberto','Campos','Barrios','89456123','Av. Los Manzanales 123', '140405','987654321','123456789','M','P','admin','admin');
+create TABLE if not exists UBIGEO(
+    CODUBI CHAR(6),
+    DISTUBI VARCHAR(100),
+    PROVUBI VARCHAR(100),
+    DPTOUBI VARCHAR(100),
+    CONSTRAINT CODUBI_PK PRIMARY KEY (CODUBI)
+);
+
+CREATE TABLE IF NOT EXISTS VIVIENDA(
+    CODVIV INT,
+    DESCVIV VARCHAR(150),
+    DIRVIV VARCHAR(150),
+    UBIGEO_CODUBI CHAR (6),
+    ESTADOVIVIENDA_CODDESTVIV INT,
+    TIPVIV_CODTIPVIV INT,
+    CONSTRAINT CODVIV_PK PRIMARY KEY (CODVIV)
+);
+
+CREATE TABLE IF NOT EXISTS ESTADO_VIVIENDA(
+    CODESTVIV INT,
+    DESCESTVIV VARCHAR(50),
+    CONSTRAINT CODESTVIV_PK PRIMARY KEY (CODESTVIV)
+);
+
+CREATE TABLE IF NOT EXISTS TIPO_VIVIENDA(
+    CODTIPVIV INT,
+    DESCTIPVIV VARCHAR(50),
+    CONSTRAINT CODTIPVIV_PK PRIMARY KEY (CODTIPVIV)
+);
+
+create table if not exists persona(
+    codper int auto_increment,
+    nomper VARCHAR(100),
+    apeper VARCHAR(100),
+    dniper char(8),
+    dirper VARCHAR(150),
+    telef1per VARCHAR(9),
+    telef2per VARCHAR(9),
+    genper char(1),
+    tipper CHAR(1),
+    useper VARCHAR(50),
+    passper VARCHAR(50),
+    ubigeo_codubi char(6),
+    CONSTRAINT codper_pk PRIMARY KEY (codper)
+);
+
+CREATE TABLE IF NOT EXISTS ALQUILER(
+    CODALQ INT,
+    FECINCALQ DATE,
+    PERSONA_PROP INT,
+    PERSONA_INQ INT,
+    CONSTRAINT CODALQ_PK PRIMARY KEY (CODALQ)
+);
+
+CREATE TABLE IF NOT EXISTS DETALLE_ALQUILER(
+    CODDETALQ INT,
+    VIVIENDA_CODVIV INT,
+    FECINICDETALQ DATE,
+    COSTDETALQ DECIMAL (8,2),
+    ALQUILER_CODALQ INT,
+    CONSTRAINT CODDETALQ_PK PRIMARY KEY (CODDETALQ)
+);
+
+/*Relacion / la tabla persona - ubigeo*/
+ALTER TABLE PERSONA
+    ADD CONSTRAINT UBIGEO_FK FOREIGN KEY (UBIGEO_CODUBI)
+    REFERENCES UBIGEO(CODUBI);
+
+/* Relacionar / la tabla Vivienda - ubogeo */
+alter table VIVIENDA
+    add constraint UBIGEO_viv_fk FOREIGN key (ubigeo_codubi)
+    REFERENCES ubigeo(CODUBI);
